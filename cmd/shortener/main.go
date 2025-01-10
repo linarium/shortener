fb60a80fd2fc3ad27624ea41d2d0235fedad078c
Morgan Lina <l.morgan@bi.zone>
2025-01-10 09:10:58 +0300

    some fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"github.com/linarium/shortener/internal/config"
 	"github.com/linarium/shortener/internal/handlers"
 	"net/http"
 )
 
 func main() {
-	cfg := config.InitConfig()
-	fmt.Println("Configuration:", cfg)
+	config.InitConfig()
 
 	r := handlers.Router()
-	fmt.Println("Server is running on port 8080...")
 	err := http.ListenAndServe(`:8080`, r)
 	if err != nil {
 		panic(err)
