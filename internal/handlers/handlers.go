79b6af159b4cad3db4ae636fb793325327e9544e
Morgan Lina <l.morgan@bi.zone>
2025-01-21 09:44:18 +0300

    [F] fix creating short url

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,13 +1,14 @@
 package handlers
 
 import (
-	"io"
-	"net/http"
-	"strings"
-
+	"encoding/json"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/linarium/shortener/internal/config"
 	"github.com/linarium/shortener/internal/service"
+	"io"
+	"net/http"
+	"strings"
 )
 
 const defaultContentType = "text/plain"
@@ -30,12 +31,6 @@ func (h *URLHandler) createShortURL(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	contentType := r.Header.Get("Content-Type")
-	if !strings.HasPrefix(contentType, defaultContentType) {
-		http.Error(w, "Incorrect content type", http.StatusBadRequest)
-		return
-	}
-
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Failed to read request body", http.StatusBadRequest)
@@ -47,13 +42,54 @@ func (h *URLHandler) createShortURL(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	shortURL := h.shortener.Shorten(string(body))
-	host := h.config.BaseURL
-	resultURL := host + "/" + shortURL
+	contentType := r.Header.Get("Content-Type")
+	var input string
 
-	w.Header().Set("Content-Type", defaultContentType)
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(resultURL))
+	if !strings.HasPrefix(contentType, defaultContentType) {
+		var data map[string]interface{}
+		err = json.Unmarshal(body, &data)
+		if err != nil {
+			http.Error(w, "Failed to parse request body", http.StatusBadRequest)
+			return
+		}
+
+		value, ok := data["url"]
+		if !ok {
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
+			return
+		}
+		input = fmt.Sprintf("%v", value)
+		w.Header().Set("Content-Type", "application/json")
+
+		shortURL := h.shortener.Shorten(input)
+		host := h.config.BaseURL
+		resultURL := host + "/" + shortURL
+
+		response := struct {
+			Result string `json:"result"`
+		}{
+			Result: resultURL,
+		}
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, "Failed to encode JSON response", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write(jsonResponse)
+
+	} else {
+		input = string(body)
+		w.Header().Set("Content-Type", defaultContentType)
+		shortURL := h.shortener.Shorten(input)
+		host := h.config.BaseURL
+		resultURL := host + "/" + shortURL
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(resultURL))
+	}
+
 }
 
 func (h *URLHandler) getURL(w http.ResponseWriter, r *http.Request) {
@@ -83,6 +119,7 @@ func Router(cfg config.Config) chi.Router {
 	handler := NewURLHandler(cfg)
 
 	r.Post("/", handler.createShortURL)
+	r.Post("/api/shorten", handler.createShortURL)
 	r.Get("/{id}", handler.getURL)
 
 	r.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
