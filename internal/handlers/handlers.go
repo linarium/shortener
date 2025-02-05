4cc5b24f8b0a44b4400df53b7154ece939c0544c
Morgan Lina <l.morgan@bi.zone>
2025-02-05 23:08:03 +0300

    fix

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -41,6 +41,7 @@ func (h *URLHandler) createJSONShortURL(w http.ResponseWriter, r *http.Request) 
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(response)
 }
 
