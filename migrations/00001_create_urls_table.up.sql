89fb98877b2072b28859afc02224010c985284df
Morgan Lina <l.morgan@bi.zone>
2025-10-04 20:55:06 +0300

    мелкие фиксы

--- migrations/00001_create_urls_table.up.sql
+++ migrations/00001_create_urls_table.up.sql
@@ -4,8 +4,10 @@ CREATE TABLE urls (
     short_url text NOT NULL UNIQUE,
     original_url text NOT NULL UNIQUE,
     user_id uuid NOT NULL,
+    correlation_id text,
     created_at timestamptz NOT NULL DEFAULT now(),
     deleted_at timestamptz
 );
 
 CREATE INDEX idx_urls_short_url ON urls(short_url);
+CREATE UNIQUE INDEX idx_urls_user_original ON urls(user_id, original_url) WHERE deleted_at IS NULL;
